48af27bce676acf77c10083e2dd6c3b0cc5facbc
ranitan <ranitanagarajan@gmail.com>
2023-09-16 10:07:10 +0530

    task

--- EventManagement/Controllers/WeddingController.cs
+++ EventManagement/Controllers/WeddingController.cs
@@ -15,13 +15,11 @@ namespace EventManagement.Controllers
         EventManagement2Entities2 EventManagementEntities = new EventManagement2Entities2();
 
 
-        // GET: Booking
-        //[HttpGet]
-        //[Authorize(Roles = "User")]
+       //GET: Booking
+       [HttpGet]
+       [Authorize(Roles = "User")]
         public ActionResult WeddingCreate()
         {
-
-         
 
             List<EventName> eventsname = EventManagementEntities.EventNames.ToList();
             ViewBag.Events = new SelectList(eventsname, "eventid", "eventname");
@@ -43,56 +41,58 @@ namespace EventManagement.Controllers
         [HttpPost]
         public ActionResult WeddingCreate([Bind(Include = "weddingdatetime,weddingdecorations,weddingtheme,weddingchairs,weddingtables,weddinghallcapacity,weddingcakes,weddinglocation,weddingeventcost,weddingbeverages,weddingPhotography,weddingStyling,weddingHospitality")] Wedding wed,DateTime weddingtime)
         {
-            //if (ModelState.IsValid)
-            //{
-            List<Wedding> bookedweddings = EventManagementEntities.Weddings.Where(w => w.weddingtime.ToString() == weddingtime.TimeOfDay.ToString()).ToList();
-            if (bookedweddings.Count > 0)
+            if (ModelState.IsValid)
             {
-                Response.Write("That time slot is unavailable");
-                List<EventName> eventsname = EventManagementEntities.EventNames.ToList();
-                ViewBag.Events = new SelectList(eventsname, "eventid", "eventname");
-
-                List<locationtable> location = EventManagementEntities.locationtables.ToList();
-                ViewBag.Location = new SelectList(location, "locationid", "locationname");
-
-                List<themetable> theme = EventManagementEntities.themetables.ToList();
-                ViewBag.Theme = new SelectList(theme, "themeid", "themename");
-
-                List<decorationtable> decor = EventManagementEntities.decorationtables.ToList();
-                ViewBag.Decor = new SelectList(decor, "decorid", "decoravailable");
-
-                List<caketable> cake = EventManagementEntities.caketables.ToList();
-                ViewBag.Cake = new SelectList(cake, "cakeid", "cakesavailable");
-
-                return View();
-            }
-            else
-            {
-                int lastUserId = EventManagementEntities.Weddings.Any() ? EventManagementEntities.Weddings.Max(u => u.id) : 0;
-
-                int? userId = Session["UserId"] as int?;
-
-                int? wedeventId = Session["eventid"] as int?;
-                if (userId.HasValue && wedeventId.HasValue)
+                List<Wedding> bookedweddings = EventManagementEntities.Weddings.Where(w => w.weddingtime.ToString() == weddingtime.TimeOfDay.ToString()).ToList();
+                if (bookedweddings.Count > 0)
                 {
-                    wed.weddinguserid = userId.Value;
-                    wed.weddingid = wedeventId.Value;
+                    Response.Write("That time slot is unavailable");
+                    List<EventName> eventsname = EventManagementEntities.EventNames.ToList();
+                    ViewBag.Events = new SelectList(eventsname, "eventid", "eventname");
 
+                    List<locationtable> location = EventManagementEntities.locationtables.ToList();
+                    ViewBag.Location = new SelectList(location, "locationid", "locationname");
+
+                    List<themetable> theme = EventManagementEntities.themetables.ToList();
+                    ViewBag.Theme = new SelectList(theme, "themeid", "themename");
+
+                    List<decorationtable> decor = EventManagementEntities.decorationtables.ToList();
+                    ViewBag.Decor = new SelectList(decor, "decorid", "decoravailable");
+
+                    List<caketable> cake = EventManagementEntities.caketables.ToList();
+                    ViewBag.Cake = new SelectList(cake, "cakeid", "cakesavailable");
+
+                    return View();
+                }
+                else
+                {
+                    int lastUserId = EventManagementEntities.Weddings.Any() ? EventManagementEntities.Weddings.Max(u => u.id) : 0;
+
+                    int? userId = Session["UserId"] as int?;
+
+                    int? wedeventId = Session["eventid"] as int?;
+                    if (userId.HasValue && wedeventId.HasValue)
+                    {
+                        wed.weddinguserid = userId.Value;
+                        wed.weddingid = wedeventId.Value;
+
+                    }
+
+                    wed.id = lastUserId + 1;
+                    wed.weddinghallcapacity = 500;
+                    wed.weddingeventcost = 1000;
+                    wed.weddingtime = weddingtime.TimeOfDay;
+
+
+
+                    EventManagementEntities.Weddings.Add(wed);
+                    EventManagementEntities.SaveChanges();
+                    return RedirectToAction("WeddingDetails", new { id = wed.id });
                 }
 
-                wed.id = lastUserId + 1;
-                wed.weddinghallcapacity = 500;
-                wed.weddingeventcost = 1000;
-                wed.weddingtime = weddingtime.TimeOfDay;
-
-
-
-                EventManagementEntities.Weddings.Add(wed);
-                EventManagementEntities.SaveChanges();
-                return RedirectToAction("WeddingDetails", new { id = wed.id });
             }
-           
-            //}
+
+            return View();
         
         }
 
@@ -147,7 +147,7 @@ namespace EventManagement.Controllers
                 }
                 catch (DbUpdateConcurrencyException ex)
                 {
-                    // Handle concurrency conflicts here
+                    
                     ModelState.AddModelError(string.Empty, "Concurrency error occurred.");
                 }
 
